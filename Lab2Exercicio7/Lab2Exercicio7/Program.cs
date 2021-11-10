da9fa14157e320a1efec96b178cfaec0d3fb7ad9
wfbelarmino <92883692+wfbelarmino@users.noreply.github.com>
2021-11-10 16:41:49 -0300

    Add files via upload

---
+++ Lab2Exercicio7/Lab2Exercicio7/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Lab2Exercicio7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double valorFracionado = 4.7;
+            int valorInteiro1 = (int)valorFracionado;
+            int valorInteiro2 = Convert.ToInt32(valorFracionado);
+            Console.WriteLine("Conversao explicita = " + valorInteiro1); Console.WriteLine("Conversao metodo Convert = " + valorInteiro2);
+
+        }
+    }
+}
