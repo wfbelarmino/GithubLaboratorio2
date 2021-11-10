fd5b7434281af97a2660243bd21d27ca5b83b1b0
wfbelarmino <92883692+wfbelarmino@users.noreply.github.com>
2021-11-10 16:25:29 -0300

    Add files via upload

---
+++ Lab2Exercicio4/Lab2Exercicio4/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Lab2Exercicio4
+{
+    class Program
+    {
+        static void Main()
+        {
+            int i = 10; float f = 0;
+            f = i; //conversão implícita, sem perda de dados. System.Console.WriteLine(f);
+            f = 0.5F;
+            i = (int)f; //conversão explícita, com perda de dados. System.Console.WriteLine(i);
+
+        }
+    }
+}
