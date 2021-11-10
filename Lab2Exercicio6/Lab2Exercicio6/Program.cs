57e53ecd1be604c16a1ecbfc1ce0b057cf58a283
wfbelarmino <92883692+wfbelarmino@users.noreply.github.com>
2021-11-10 16:36:19 -0300

    Add files via upload

---
+++ Lab2Exercicio6/Lab2Exercicio6/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Lab2Exercicio6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string stringInteiro = "123456789"; int valorStringInteiro;
+            bool conversao1 = Int32.TryParse(stringInteiro, out valorStringInteiro); 
+            Console.WriteLine("Conversão efetuada:" + conversao1 + " Valor: " + valorStringInteiro);
+
+            string stringInteiroGrande = "999999999999999999999999999999999999999999999"; 
+            int valorStringInteiroGrande;
+            bool conversao2 = Int32.TryParse(stringInteiroGrande, out valorStringInteiroGrande); 
+            Console.WriteLine("Conversão efetuada:" + conversao2 + " Valor: " + valorStringInteiroGrande); 
+            string stringLetras = "abc";
+            double valorStringLetras;
+            bool conversao3 = Double.TryParse(stringLetras, out valorStringLetras); Console.WriteLine("Conversão efetuada:" + conversao3 + " Valor: " + valorStringLetras);
+
+        }
+    }
+}
