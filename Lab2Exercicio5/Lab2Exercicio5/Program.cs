dfd5f6cd05b185e546f748cd947ed2c64839ff9a
wfbelarmino <92883692+wfbelarmino@users.noreply.github.com>
2021-11-10 16:34:36 -0300

    Add files via upload

---
+++ Lab2Exercicio5/Lab2Exercicio5/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Lab2Exercicio5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string stringInteiro = "123456789";
+            int valorStringInteiro = Convert.ToInt32(stringInteiro); Console.WriteLine(valorStringInteiro);
+            Int64 valorInt64 = 123456789;
+            int valorInt = Convert.ToInt32(valorInt64); Console.WriteLine(valorInt);
+            string stringInteiroGrande = "999999999999999999999999999999999999999999999"; 
+            int valorStringInteiroGrande = Convert.ToInt32(stringInteiroGrande);
+
+        }
+    }
+}
