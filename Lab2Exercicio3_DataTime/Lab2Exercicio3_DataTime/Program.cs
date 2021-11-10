29f404a137c2fed75f97b6fd05a8358aec350876
wfbelarmino <92883692+wfbelarmino@users.noreply.github.com>
2021-11-10 16:24:29 -0300

    Add files via upload

---
+++ Lab2Exercicio3_DataTime/Lab2Exercicio3_DataTime/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Lab2Exercicio3_DataTime
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DateTime dt = new DateTime(); 
+            Console.WriteLine(dt);
+        }
+    }
+}
