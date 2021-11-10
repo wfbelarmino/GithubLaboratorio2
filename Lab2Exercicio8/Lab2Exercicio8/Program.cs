cb6f2eb9a51688c254ad320259305ca64db69c2d
wfbelarmino <92883692+wfbelarmino@users.noreply.github.com>
2021-11-10 16:42:54 -0300

    Add files via upload

---
+++ Lab2Exercicio8/Lab2Exercicio8/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Lab2Exercicio8
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x = 10; //declara o valor onteiro
+            double y = 3.4; //declara valor real
+            Console.WriteLine("{0} {1}", x, y); // O comando WriteLine exibe os resultados na linha abaixo.
+
+        }
+    }
+}
